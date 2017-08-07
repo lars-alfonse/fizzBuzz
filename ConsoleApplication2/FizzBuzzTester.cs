551756f0b0853862fe9f13c6aaddb46aec90982a
lars-alfonse <lars.alfonse@gmail.com>
2017-08-07 07:14:41 -0500

    fizzBuzz made

---
+++ ConsoleApplication2/FizzBuzzTester.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class FizzBuzzTester
+    {
+        public void checkNumber()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if(i == 1)
+                {
+                    Console.WriteLine("1");
+                }
+                else if (checkForMultiples(i , 3) && checkForMultiples(i, 5))
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if(checkForMultiples(i, 3))
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (checkForMultiples(i, 5))
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+        private bool checkForMultiples(int number, int divisor )
+        {
+            double result = number % divisor;
+            if(result == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
